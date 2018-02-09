d0ebe558695bb65f0240797e4c37825d7062da7e
nine <nine.yang.coding@gmail.com>
2018-02-09 11:51:54 +0800

    todo:node data

--- Main.py
+++ Main.py
@@ -1,3 +1,8 @@
+from hashlib import md5
+from struct import unpack_from
+from collections import OrderedDict
+
+
 class ConsistentHash:
     """
     :nodes 节点
@@ -5,28 +10,70 @@ class ConsistentHash:
     """
 
     def __init__(self, nodes, virtual=3):
-        self.actual_nodes = self.base_nodes = nodes[:]
+        self.base_nodes = nodes[:]
+        self.actual_nodes = {}
         self.virtual = virtual
+        # 这里的rings相当于整个环，几个节点所有数据的集合，这里只是为了方便演示
+        self.rings = {}
         for node in nodes:
-            self.actual_nodes += self.addNode(node)
-
-        print(self.actual_nodes)
+            self.addNode(node)
+        # 这里按照key做一次排序，方便比较其大小
+        self.actual_nodes = OrderedDict(sorted(self.actual_nodes.items(), key=lambda t: t[0]))
 
     def addNode(self, node):
-        tmp = []
+        # todo 这里还需要迁移数据
+        self.actual_nodes[self.hash(node)] = node
         for item in range(self.virtual):
-            tmp.append(node + '#' + str(item))
+            tmp = node + '#' + str(item)
+            self.actual_nodes[self.hash(tmp)] = tmp
 
-        return tmp
-
-    def hash(self):
+    def removeNode(self, node):
+        # todo 迁移数据
         pass
+
+    def hash(self, value):
+        return unpack_from(">I", md5(str(value).encode('utf-8')).digest())[0]
+
+    '''
+    删除key
+    '''
+
+    def addKey(self, key, value):
+        node = self.__findKeyWhereIn(key)
+        # 需要处理的添加业务逻辑(为方便处理和演示，我这里先用一个全局的ring代替，线上环境应该真实的存放于对应的节点中)
+        self.rings[key] = value
+        return node
+
+    '''
+    添加key
+    '''
+
+    def removeKey(self, key):
+        node = self.__findKeyWhereIn(key)
+        # 需要处理的删除业务逻辑(为方便处理和演示，我这里先用一个全局的ring代替，线上环境应该删除真实节点存放的数据)
+        self.rings.pop(key)
+        pass
+
+    '''
+    查找key应该存放在哪个节点
+    '''
+
+    def __findKeyWhereIn(self, key):
+        hash = self.hash(key)
+        for item in self.actual_nodes.keys():
+            if hash < item:
+                return self.actual_nodes[item]
 
 
 if __name__ == "__main__":
     servers = [
-        'node1',
-        'node2',
-        'node3'
+        '192.168.1.1',
+        '192.168.1.2',
+        '192.168.1.3'
     ]
     consistentHash = ConsistentHash(servers)
+    print(consistentHash.rings)
+    consistentHash.addKey('name', 'nine')
+    print(consistentHash.rings)
+    consistentHash.removeKey('name')
+    print(consistentHash.rings)
